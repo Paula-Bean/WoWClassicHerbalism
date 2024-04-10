ce69b996bd628f265df260d6fadb10645d5b9fb2
Paula Bean <paula.j.bean@partijvoordeinsecten.nl>
2024-04-10 15:57:09 +0200

    Initial commit

---
+++ herbalism.py
@@ -0,0 +1,168 @@
+
+info = """
+1 25 50 100
+Peacebloom
+Found in open terrain: Darkshore, Dun Morogh, Durotar, Elwynn Forest, Loch Modan, Mulgore, Redridge Mountains, Silverpine Forest, Teldrassil, The Barrens, Tirisfal Glades, Westfall
+
+1 25 50 100
+Silverleaf
+Found near trees: Darkshore, Dun Morogh, Durotar, Elwynn Forest, Loch Modan, Mulgore, Redridge Mountains, Silverpine Forest, Teldrassil, The Barrens, Tirisfal Glades, Westfall
+
+15 40 65 115
+Earthroot
+Found on hills or near cliffs: Darkshore, Dun Morogh, Durotar, Elwynn Forest, Loch Modan, Mulgore, Redridge Mountains, Silverpine Forest, Teldrassil, The Barrens, Tirisfal Glades, Wailing Caverns, Westfall
+
+50 75 100 150
+Mageroyal
+Found in open terrain: Ashenvale, Darkshore, Durotar, Duskwood, Hillsbrad Foothills, Loch Modan, Redridge Mountains, Silverpine Forest, Stonetalon Mountains, Teldrassil, The Barrens, Westfall, Wetlands
+
+70 95 120 170
+Briarthorn
+Found near trees: Ashenvale, Darkshore, Duskwood, Hillsbrad Foothills, Loch Modan, Razorfen Kraul, Redridge Mountains, Silverpine Forest, The Barrens, Westfall, Wetlands
+
+85 110 135 185
+Stranglekelp
+Near or under water: Alterac Mountains, Arathi Highlands, Ashenvale, Azshara, Blackfathom Deeps, Darkshore, Desolace, Dustwallow Marsh, Feralas, Hillsbrad Foothills, Maraudon, Silverpine Forest, Stranglethorn Vale, Swamp of Sorrows, Tanaris, The Barrens, The Hinterlands, Westfall, Wetlands
+
+100 125 150 200
+Bruiseweed
+Near buildings or hills: Alterac Mountains, Arathi Highlands, Ashenvale, Blackfathom Deeps, Darkshore, Desolace, Hillsbrad Foothills, Loch Modan, Redridge Mountains, Silverpine Forest, Stonetalon Mountains, The Barrens, Thousand Needles, Wailing Caverns, Westfall, Wetlands
+
+115 140 165 215
+Wild Steelbloom
+Found on hills or near cliffs: Alterac Mountains, Arathi Highlands, Ashenvale, Badlands, Desolace, Duskwood, Hillsbrad Foothills, Stonetalon Mountains, Stranglethorn Vale, The Barrens, Thousand Needles, Wetlands
+
+120 145 170 220
+Grave Moss
+Near crypts and graveyards: Alterac Mountains, Arathi Highlands, Desolace, Duskwood, Razorfen Downs, Scarlet Monastery (Graveyard), The Barrens, Wetlands
+
+125 150 175 225
+Kingsblood
+Found in open terrain: Alterac Mountains, Arathi Highlands, Ashenvale, Badlands, Desolace, Duskwood, Dustwallow Marsh, Hillsbrad Foothills, Scarlet Monastery (Graveyard), Stonetalon Mountains, Stranglethorn Vale, Swamp of Sorrows, The Barrens, Thousand Needles, Wailing Caverns, Wetlands
+
+150 175 200 250
+Liferoot
+Near water sources: Alterac Mountains, Arathi Highlands, Ashenvale, Desolace, Dustwallow Marsh, Feralas, Hillsbrad Foothills, Scarlet Monastery (Cathedral, Library), Silverpine Forest, Stranglethorn Vale, The Hinterlands, Wailing Caverns, Wetlands
+
+160 185 210 260
+Fadeleaf
+Found near trees: Alterac Mountains, Arathi Highlands, Badlands, Dustwallow Marsh, Razorfen Kraul, Scarlet Monastery (Armory, Library), Stranglethorn Vale, Swamp of Sorrows, The Hinterlands
+
+170 195 220 270
+Goldthorn
+Found on hills or near cliffs: Alterac Mountains, Arathi Highlands, Azshara, Badlands, Blasted Lands, Dustwallow Marsh, Feralas, Razorfen Downs, Scarlet Monastery (Cathedral), Stranglethorn Vale, Swamp of Sorrows, The Hinterlands
+
+185 210 235 285
+Khadgar's Whisker
+Found near trees: Alterac Mountains, Arathi Highlands, Azshara, Badlands, Dustwallow Marsh, Feralas, Hillsbrad Foothills, Stranglethorn Vale, Swamp of Sorrows, The Hinterlands
+
+195 220 245 295
+Wintersbite
+Found in open terrain: Alterac Mountains
+
+205 230 255 305
+Firebloom
+Found in open terrain: Badlands, Blasted Lands, Searing Gorge, Tanaris
+
+210 235 260 310
+Purple Lotus
+Found in or near ruins: Ashenvale, Azshara, Badlands, Feralas, Stranglethorn Vale, Tanaris, The Hinterlands, Zul'Gurub
+
+220 245 270 320
+Arthas' Tears
+Found in open terrain: Eastern Plaguelands, Felwood, Razorfen Downs, Western Plaguelands
+
+230 255 280 330
+Sungrass
+Found in open terrain: Azshara, Blasted Lands, Burning Steppes, Eastern Plaguelands, Felwood, Feralas, Silithus, The Hinterlands, Un'Goro Crater, Western Plaguelands, Zul'Gurub
+
+235 260 285 335
+Blindweed
+Found near water sources: Maraudon, Swamp of Sorrows, Un'Goro Crater
+
+245 270 295 345
+Ghost Mushroom
+Found inside of caves: Dire Maul (East), Maraudon, The Hinterlands
+
+250 275 300 350
+Gromsblood
+Found in open terrain: Ashenvale, Blasted Lands, Desolace, Dire Maul (East), Felwood
+
+260 285 310 360
+Golden Sansam
+Found in open terrain: Azshara, Burning Steppes, Eastern Plaguelands, Felwood, Feralas, Silithus, The Hinterlands, Un'Goro Crater, Zul'Gurub
+
+270 290 315 370
+Dreamfoil
+Found in open terrain: Azshara, Burning Steppes, Dire Maul (East), Eastern Plaguelands, Felwood, Silithus, Un'Goro Crater, Western Plaguelands, Zul'Gurub
+
+280 305 330 380
+Mountain Silversage
+Found in high locations and on cliffs: Azshara, Burning Steppes, Eastern Plaguelands, Felwood, Silithus, Un'Goro Crater, Western Plaguelands, Winterspring, Zul'Gurub
+
+285 310 335 385
+Plaguebloom
+Found in open terrain: Eastern Plaguelands, Felwood, Western Plaguelands
+
+290 315 340 390
+Icecap
+Found on hills or near cliffs: Winterspring
+
+300 325 350 400
+Black Lotus
+Found in open terrain: Burning Steppes, Eastern Plaguelands, Silithus, Winterspring
+"""
+
+herbalist_skill_level = 226 # Train at herbalism skill levels ..., 225
+
+herbs = {} # Mapping of herb name => (orange, yellow, green, gray skill level, where)
+
+for line in info.splitlines():
+    line = line.strip()
+    if line == "":
+        parsephase = 0
+        continue
+    if parsephase == 0:
+        skill = line
+        parsephase = 1
+    elif parsephase == 1:
+        herb = line
+        skills = skill.split(" ")
+        orange, yellow, green, gray = [int(skill) for skill in skills]
+        herbs[herb] = [orange, yellow, green, gray, ""]
+        parsephase = 2
+    elif parsephase == 2:
+        herbs[herb][4] = line
+        parsephase = 3
+
+
+print(f"\nFeasible herbs for a leveling herbalist at level {herbalist_skill_level}:\n")
+
+for herb, skills in sorted(herbs.items()):
+    orange, yellow, green, gray, where = skills
+    if herbalist_skill_level < orange:
+        continue
+    if herbalist_skill_level >= gray:
+        continue
+
+    color = ""
+    if  herbalist_skill_level < yellow:
+        color = "orange"
+    elif herbalist_skill_level < green:
+        color = "yellow"
+    elif herbalist_skill_level < gray:
+        color = "green"
+
+    print(f"{herb} ({color}): {orange}, {yellow}, {green}, {gray}")
+
+print("\nWhere it can be found:\n")
+
+for herb, skills in sorted(herbs.items()):
+    orange, yellow, green, gray, where = skills
+    if herbalist_skill_level < orange:
+        continue
+    if herbalist_skill_level >= gray:
+        continue
+    print(f"{herb}: {where}\n")
+
+print("\nRaadpleeg https://www.gamermaps.net/world-of-warcraft/map/ waar je de herbs precies kunt vinden")
